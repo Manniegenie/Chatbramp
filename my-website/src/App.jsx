6c329793d6b8353d5fa11277d43c29a1a706a6e1
Manniegenei <Manuelwurld@gmail.com>
2025-08-28 00:06:49 +0100

    uer

---
+++ my-website/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './pages/Home';  // Import your Home component
+
+function App() {
+  return (
+    <div className="App">
+      <Home />  {/* Render Home component */}
+    </div>
+  );
+}
+
+export default App;
